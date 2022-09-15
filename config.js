685ca6be2351c0b816b7a4c8485d9b865e68bb15
ModsBotz <81307290+zero-spec@users.noreply.github.com>
2022-09-15 16:34:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
-global.packname = 'AzzBot√'
-global.author = 'AzzBot-MD'
-global.sessionName = 'Xavior'
+global.owner = ['6289505329516']
+global.premium = ['6289505329516']
+global.packname = 'ZxBotz√'
+global.author = 'ZxBotz-MD'
+global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
